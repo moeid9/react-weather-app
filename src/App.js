5a794fc4815cfb85bc4d5047ae615695297e8113
Moeid Haider <moeidhaider9@gmail.com>
2023-10-05 14:27:53 +0100

    implemented header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import { useEffect , useState } from 'react';
 import './App.css';
-
+import Header from './components/header';
 
 
 function App() {
 
+  return (
+    <div className='main'>
+     <Header/>
+    </div>
+  );
 }
 
 export default App;
